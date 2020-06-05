2a2af3fedced32e14c21129673ba41effe55c58d
Jung <junghong91@naver.com>
2020-06-05 15:31:17 +0900

    Making a Stage by array(12x20) and Setting the Tetrominos blocks and color by array

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -2,10 +2,17 @@ import React from "react";
 
 import Cell from "./Cell";
 
-const Stage = ({ stage }) => (
-  <div>
-    <Cell />
-  </div>
-);
+const Stage = ({ stage }) => {
+  console.log(stage);
+  return (
+    <div>
+      {stage.map((row) => {
+        return row.map((cell, x) => {
+          return <Cell key={x} type={cell[0]} />;
+        });
+      })}
+    </div>
+  );
+};
 
 export default Stage;
