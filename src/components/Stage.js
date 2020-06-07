96d44d88f81787019abc5b90de36a49a98ebb118
Jung <junghong91@naver.com>
2020-06-07 22:43:45 +0900

    Styling of playing fields by using styled component

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,17 +1,17 @@
 import React from "react";
+import { StyledStage } from "./styles/StyledStage";
 
 import Cell from "./Cell";
 
 const Stage = ({ stage }) => {
-  console.log(stage);
   return (
-    <div>
+    <StyledStage width={stage[0].length} height={stage.length}>
       {stage.map((row) => {
         return row.map((cell, x) => {
           return <Cell key={x} type={cell[0]} />;
         });
       })}
-    </div>
+    </StyledStage>
   );
 };
 
---
+++ src/components/styles/StyledStartButton.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledStartButton = styled.div`
+  box-sizing: border-box;
+  margin: 0 0 20px 0;
+  padding: 20px;
+  min-height: 30px;
+  width: 100%;
+  border-radius: 20px;
+  border: none;
+  color: white;
+  background: #333;
+  font-family: Dekar-Regular, Arial, Helvetica, sans-serif;
+  font-size: 1.2rem;
+  outline: none;
+  cursor: pointer;
+  text-transform: uppercase;
+  text-align: center;
+`;
