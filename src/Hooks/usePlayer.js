b293fbc7950c5ac26505233e8ef52a173c5676ad
Jung <junghong91@naver.com>
2020-06-09 14:21:17 +0900

    rotating the tetromino

--- src/Hooks/usePlayer.js
+++ src/Hooks/usePlayer.js
@@ -1,7 +1,7 @@
 import { useState, useCallback } from "react";
 
 import { TETROMINOS, randomTetromino } from "../tetrominos";
-import { STAGE_WIDTH } from "../gameHelpers";
+import { STAGE_WIDTH, checkCollision } from "../gameHelpers";
 
 export const usePlayer = () => {
   // Initial set of player
@@ -11,8 +11,40 @@ export const usePlayer = () => {
     collided: false,
   });
 
+  const rotate = (matrix, direction) => {
+    // Make the rows to colums(transpose)
+    const rotatedTetro = matrix.map((_, index) =>
+      matrix.map((column) => column[index])
+    );
+    // Reverse each row to get a rotated matrix
+    if (direction > 0) {
+      return rotatedTetro.map((row) => row.reverse());
+    }
+    return rotatedTetro.reverse();
+  };
+
+  // Checking collision
+  const playerRotate = (stage, direction) => {
+    // Copy the player completly
+    const clonedPlayer = JSON.parse(JSON.stringify(player));
+    clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, direction);
+
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+    while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + (offset > 0 ? 1 : -1));
+      if (offset > clonedPlayer.tetromino[0].length) {
+        rotate(clonedPlayer.tetromino, -direction);
+        clonedPlayer.pos.x = pos;
+        return;
+      }
+    }
+
+    setPlayer(clonedPlayer);
+  };
+
   const updatePlayerPos = ({ x, y, collided }) => {
-    console.log(x, y, collided);
     setPlayer((prev) => ({
       ...prev,
       pos: { x: (prev.pos.x += x), y: (prev.pos.y += y) },
@@ -28,5 +60,5 @@ export const usePlayer = () => {
     });
   }, []);
 
-  return [player, updatePlayerPos, resetPlayer];
+  return [player, updatePlayerPos, resetPlayer, playerRotate];
 };
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -18,7 +18,7 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
   const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log("re-render");
@@ -65,6 +65,8 @@ const Tetris = () => {
       } else if (keyCode === 40) {
         // Down key pressed
         dropPlayer();
+      } else if (keyCode === 38) {
+        playerRotate(stage, 1);
       }
     }
   };
